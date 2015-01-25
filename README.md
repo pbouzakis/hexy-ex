12ba414a57a9592bd1dc68a34b9f8393d64dbe37
Paul Bouzakis <github@paulbouzakis.com>
2015-01-25 09:48:33 -0500

    Add our first domain entity w/ test.

--- README.md
+++ README.md
@@ -14,3 +14,38 @@ source code written in ES6 (aka ES 2015)
 ========================================
 Using 6to5 to transpile source into valid ES5. 
 Friendly FYI :)
+
+Entity State
+============
+Entities are injected with a state object.
+This object is live meaning that it provides hooks for application adapters
+to manage (most often is for the storage adapter to save the data).
+
+Therefore when using the state object, when getting/setting values you must always
+reference off of the state object otherwise the data is copied by value to the new var, 
+leaving the state with the old value, and therefore not able to hook into the change.
+
+
+Example:
+```
+function createEntity(_state) {
+    return {
+        makeThatChange(size) {
+            _state.size = size; // Good
+
+            var _size = _state.size;
+            _size = size; // Bad
+        }
+    };
+}
+```
+
+If this too much of a leaky abstraction, an alternative solution might be to just
+use property objects, where aliasing is less likely.
+
+```
+var size = _state.size;
+
+size.set(newValue);
+size.get();
+```
---
+++ test/domain/placeToEat.js
@@ -0,0 +1,51 @@
+"use strict";
+
+import placeToEat from "../../lib/domain/placeToEat";
+
+describe("Domain entity: A `placeToEat`", () => {
+    beforeEach(() => {
+        this.place = placeToEat({ 
+            id: 1, 
+            name: "Pizza House", 
+            cuisine: "American" 
+        });
+    });
+
+    it("is selectable.", () => {
+        this.place.should.respondTo("select");
+    });
+
+    it("has a name.", () => {
+        this.place.name.should.equal("Pizza House");
+    });
+
+    it("has a cuisine.", () => {
+        this.place.cuisine.should.equal("American");
+    });
+
+    it("has never been selected.", () => {
+        this.place.numOfTimesSelected.should.equal(0);
+    });
+
+    describe("when selected", () => {
+        beforeEach(() => {
+            this.place.select();
+        });
+
+        it("has been selected once", () => {
+            this.place.numOfTimesSelected.should.equal(1);
+        });
+
+        describe("and when selected multiple times (say 3)", () => { // Why not, to be safe :)
+            beforeEach(() => {
+                this.place.select();
+                this.place.select();
+                this.place.select();
+            });
+
+            it("has been selected four times.", () => {
+                this.place.numOfTimesSelected.should.equal(4);
+            });
+        });
+    });
+});
